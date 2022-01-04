07afd0ca3e96efc0c8090febc0aa6e39a9499e8c
muthamizharasiv <muthamizharasi.v@collabera.com>
2022-01-04 12:26:14 +0530

    java

---
+++ java-simple-app/src/test/java/JavaBasicTests.java
@@ -0,0 +1,17 @@
+public class JavaBasicTests {
+    public void testVariables(){
+        int age = 10;
+        float sal = 20.6f;
+        double annualSal = 56.56;
+        char ch = 'a';
+        boolean isManager = false;
+        byte byt = 10;
+        short sh = 10;
+        long lg = 4343554345444L;
+        String nm = "android";
+
+    }
+    public  void testMethods(){
+
+    }
+}
