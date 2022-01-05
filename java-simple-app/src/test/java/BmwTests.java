01deae0177140531937397acda08339e37393e4d
muthamizharasiv <muthamizharasi.v@collabera.com>
2022-01-05 09:04:52 +0530

    java recap:learning variable\using unit test

--- java-simple-app/src/test/java/BmwTests.java
+++ java-simple-app/src/test/java/BmwTests.java
@@ -1,4 +1,5 @@
 import Clsobj.Bmw;
+import Clsobj.Car;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,21 @@ public class BmwTests {
         int expectedSpeed = 10;
         int actualSpeed = bmw.speed;
         Assertions.assertEquals(expectedSpeed,actualSpeed);
+    }
+    @DisplayName("Test is Bmw is a car")
+    @Test
+    void testBmwObjectCar(){
+        Bmw bmw = new Bmw();
+        boolean expected = true;
+        boolean actual = bmw instanceof  Car;
+        Assertions.assertEquals(expected,actual);
+    }
+    @DisplayName("Test Car is Not a BMW")
+    @Test
+    void testCarObjectNotBmw(){
+        Car car = new Car();
+        boolean expected = false;
+        boolean actual = car instanceof Bmw;
+        Assertions.assertEquals(expected,actual);
     }
 }
