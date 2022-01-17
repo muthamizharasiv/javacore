1f83d078c16803ef414df7a006044ab934e24fda
muthamizharasiv <muthamizharasi.v@collabera.com>
2022-01-17 17:10:38 +0530

    microservice : inter service communication

---
+++ week3/pqrmicroservice/pqrmicroservice/src/main/java/com/ani/pqr/PqrmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ani.pqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrmicroserviceApplication.class, args);
+	}
+
+}
---
+++ week3/pqrmicroservice/pqrmicroservice/src/test/java/com/ani/pqr/PqrmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.pqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqrmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
