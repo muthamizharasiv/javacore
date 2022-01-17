9dae1c8574926c9e63a06a50cae32281de8a89d8
muthamizharasiv <muthamizharasi.v@collabera.com>
2022-01-17 17:07:31 +0530

    microservice : inter service communication

---
+++ week3/abc-microservice (1)/abc-microservice/src/main/java/com/ani/abc/abcmicroservice/AbcMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ani.abc.abcmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcMicroserviceApplication.class, args);
+	}
+
+}
